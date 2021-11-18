22a69276ebcf06b13736a4122bf293325d9115a2
Junhobu <68681792+Junhobu@users.noreply.github.com>
2021-11-19 02:47:13 +0900

    Modify to java class

--- app/src/main/java/com/example/sexydynamite/MainActivity.java
+++ app/src/main/java/com/example/sexydynamite/MainActivity.java
@@ -14,11 +14,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carmera_certification);
 
-        Button imageButton =(Button) findViewById(R.id.btn1);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        Button imageButton1 =(Button) findViewById(R.id.btn1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),SubActivity.class);
+                Intent intent=new Intent(getApplicationContext(), cameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton2 =(Button) findViewById(R.id.btn2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), listActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton3 =(Button) findViewById(R.id.btn3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), gameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton4 =(Button) findViewById(R.id.btn4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), exActivity.class);
                 startActivity(intent);
             }
         });
