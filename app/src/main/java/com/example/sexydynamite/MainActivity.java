1cba74d254f1240716b4c251780dbcaf27944e37
Junhobu <68681792+Junhobu@users.noreply.github.com>
2021-11-19 02:38:45 +0900

    Modify to camera

--- app/src/main/java/com/example/sexydynamite/MainActivity.java
+++ app/src/main/java/com/example/sexydynamite/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.sexydynamite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ex);
+        setContentView(R.layout.activity_carmera_certification);
+
+        Button imageButton =(Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
