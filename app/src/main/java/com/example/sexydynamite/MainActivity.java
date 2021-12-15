02cf2b92c3e53fc2b4173d5bde42ce703385c089
BumGom <jjh33055@naver.com>
2021-12-15 20:29:17 +0900

    asdf

--- app/src/main/java/com/example/sexydynamite/MainActivity.java
+++ app/src/main/java/com/example/sexydynamite/MainActivity.java
@@ -7,21 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
+    //view Objects
+    private Button buttonScan;
+    private TextView textViewName, textViewAddress, textViewResult;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carmera_certification);
-
-        /*Button imageButton1 =(Button) findViewById(R.id.btn1);
-        imageButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), cameraActivity.class);
-                startActivity(intent);
-            }
-        });*/
 
         Button imageButton2 =(Button) findViewById(R.id.btn2);
         imageButton2.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +58,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //View Objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //button onClick
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //scan option
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+            }
+        });
     }
 
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+                Toast.makeText(MainActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                //qrcode 결과가 있으면
+                Toast.makeText(MainActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+                    textViewName.setText(obj.getString("name"));
+                    textViewAddress.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_LONG).show();
+                    textViewResult.setText(result.getContents());
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
