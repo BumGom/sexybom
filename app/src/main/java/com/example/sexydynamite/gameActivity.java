65b7cd9bf9fd61f0c1834b5b12740939857bba8e
Junhobu <68681792+Junhobu@users.noreply.github.com>
2021-12-16 00:54:22 +0900

    동영상 재생완료

--- app/src/main/java/com/example/sexydynamite/gameActivity.java
+++ app/src/main/java/com/example/sexydynamite/gameActivity.java
@@ -1,25 +1,52 @@
 package com.example.sexydynamite;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gameActivity extends AppCompatActivity {
+    VideoView vv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Button gameButton=(Button)findViewById(R.id.dnagame);
+        /*Button gameButton=(Button)findViewById(R.id.dnagame);
         gameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),UnityPlayerActivity.class);
                 startActivity(intent);
+            }
+        });*/
+
+        vv= findViewById(R.id.vv);
+        //Video Uri
+        Uri videoUri= Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+
+        videoUri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.dnagame);
+        //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
+        vv.setMediaController(new MediaController(this));
+
+        //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
+        vv.setVideoURI(videoUri);
+
+        //동영상을 읽어오는데 시간이 걸리므로..
+        //비디오 로딩 준비가 끝났을 때 실행하도록..
+        //리스너 설정
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //비디오 시작
+                vv.start();
             }
         });
     }
